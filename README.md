4d26e1ba9469e848889140d91c825eef418d1893
Owen Dechow <owen.dechow@gmail.com>
2025-05-30 22:16:07 -0400

    Update functions

--- README.md
+++ README.md
@@ -3,17 +3,40 @@
 Those of us who use Neovim but cannot spell spend entirely too much time fighting with
 ourselves and not enough time fighting our code. Neovim's built in spelling system is ok,
 but it lacks some features that I would like to see. This becomes more evident when you
-are using Treesitter. Most of the time only strings and comments get checked for
-spelling, and spelling issues show up as a little line instead of a real warning or info
-bar. WurdSpel works to fix this by running spell check on everything, not just comments.
-This means that for the few of us that can even spell our variable names right, we are
-saved. WurdSpel also makes spelling errors warnings (this can be changed if you aren't
-as bad as spelling as me). To the spelling failures amongst us, I present
-`nvim_wurd_spel`
+are using Treesitter. Most of the time only strings and comments get checked for spelling,
+and spelling issues show up as a little line instead of a real warning or info bar.
+WurdSpel works to fix this by running spell check on everything, not just comments. This
+means that for the few of us that can even spell our variable names right. We are saved!
+WurdSpel also makes spelling errors warnings (this can be changed if you aren't as bad as
+spelling as me). To the spelling failures amongst us, I present `nvim_wurd_spel`
+
+## User Commands
+
+WurdSpel Adds the following user commands
+
+`WurdSpelSuggest`
+
+    Gives spelling suggestions and the option to add the misspelled word to user settings.
+
+`WurdSpelGood`
+
+    Adds the misspelled word to user settings.
+
+`WurdSpelBad`
+
+    Marks the word under the cursor as misspelled.
+
+`WurdSpelBuf`
+
+    Runs a spell check over entire buffer.
+
+`WurdSpelToggle`
+
+    Enables or disables WurdSpel.
 
 ## Installation
 
-[lazy.nvim](https://github.com/folke/lazy.nvim)
+[lazy.nvim](https://github.com/folke/lazy.nvim) - Default config is commented out.
 ```lua
 return {
     "Owen-Dechow/nvim_wurd_spel",
@@ -31,8 +54,24 @@ return {
             -- -- IMPORTENT: words added to Neovim's built in
             --    spelling ignore dictionary are still ignored.`
             --    It is suggested to add words to that dictionary
-            --    using `zg` or `:spellgood` instead of adding
-            --    words here.
+            --    using `zg`, `:spellgood`, or from the WurdSpelSuggest
+            --    options instead of adding words here.
+
+            -- enabled = true
+            -- -- Enable WurdSpel on start
+
+            -- remap = true
+            -- -- Remap the builtin z=, zg, & zw commands to the
+            -- -- WurdSpel command equivalents.
+            -- -- z= -> WurdSpelSuggest
+            -- -- zg -> WurdSpelGood
+            -- -- zw -> WurdSpelBad
+
+            -- remap_special = false
+            -- -- Add special remaps
+            -- -- <leader>zz -> WurdSpelSuggest
+            -- -- <leader>zg -> WurdSpelGood
+            -- -- <leader>zw -> WurdSpelBad
         })
     end
 }
--- lua/wurd_spel/init.lua
+++ lua/wurd_spel/init.lua
@@ -1,14 +1,48 @@
----@diagnostic disable: undefined-global
+---@diagnostic disable: undefined-global, unnecessary-if
 
 local M = {
-    attached_bufs = {}
+    attached_bufs = {},
 }
 
 M.config = {
     severity = vim.diagnostic.severity.WARN,
     min_length = 5,
-    ignore = { "Dechow", "Neovim" }
+    ignore = { "Dechow", "Neovim" },
+    enabled = true,
+    remap = true,
+    remap_special = false,
 }
+
+local function get_diagnostic_for_namespace(namespace_id)
+    local pos = vim.api.nvim_win_get_cursor(0)
+    local line = pos[1] - 1
+    local col = pos[2]
+
+    local diagnostics = vim.diagnostic.get(0, { namespace = namespace_id, lnum = line })
+    for _, diag in ipairs(diagnostics) do
+        if col >= diag.col and col <= diag.end_col then
+            return diag
+        end
+    end
+end
+
+local function get_word_under_cursor()
+    local cursor_pos = vim.api.nvim_win_get_cursor(0)
+    local bufn = vim.api.nvim_get_current_buf()
+    local line = vim.api.nvim_buf_get_lines(bufn, cursor_pos[1] - 1, cursor_pos[1], false)[1]
+    local word_map = {}
+
+    for w in string.gmatch(line, "[A-Z]?[a-z]+") do
+        local i = word_map[w] or 0
+        word_map[w] = string.find(line, w, i)
+
+        if word_map[w] - 1 <= cursor_pos[2] and word_map[w] - 1 + string.len(w) > cursor_pos[2] then
+            return w
+        end
+
+        word_map[w] = word_map[w] + 1
+    end
+end
 
 function M.check_string_content(bufn, content, offset)
     local diagnostics = {}
@@ -29,27 +63,14 @@ function M.check_string_content(bufn, content, offset)
                 if e[2] == "bad" then
                     local i = word_map[w] or 0
                     word_map[w] = string.find(line, w, i)
-                    local suggestions = vim.fn.spellsuggest(w);
-                    local suggest = ""
-                    if #suggestions >= 3 then
-                        suggest = table.concat(suggestions, ", ", 1, 3)
-                    else
-                        if #suggestions > 0 then
-                            suggest = table.concat(suggestions, ", ", 1, #suggestions)
-                        end
-                    end
-
 
                     diagnostics[#diagnostics + 1] = {
                         lnum = idx - 1 + offset,
                         col = word_map[w] - 1,
                         end_col = word_map[w] + string.len(w) - 1,
                         severity = M.config.severity,
-                        message = "Possible misspelling: \""
-                            .. e[1]
-                            .. "\" (Maybe: "
-                            .. suggest
-                            .. ")"
+                        message = "Possible misspelling: \"" .. w .. "\".",
+                        word = w
                     }
 
                     word_map[w] = word_map[w] + 1
@@ -81,7 +102,80 @@ function M.spell_check_buffer()
     M.check_string_content(bufn, content, 0)
 end
 
+function M.spellbad()
+    local w = get_word_under_cursor()
+    if w then
+        vim.cmd("spellwrong " .. w)
+        M.spell_check_buffer()
+    else
+        vim.notify("No word under cursor.")
+    end
+end
+
+function M.spellgood()
+    local diag = get_diagnostic_for_namespace(M.ns_id)
+    if diag then
+        vim.cmd("spellgood " .. diag.word)
+        M.spell_check_buffer()
+    else
+        vim.notify("No WurdSpel diagnostic under cursor.")
+    end
+end
+
+function M.spellsuggest()
+    local diag = get_diagnostic_for_namespace(M.ns_id)
+    if diag then
+        local suggestions = vim.fn.spellsuggest(diag.word);
+        local add_to_list = "[Add to user settings]"
+        table.insert(suggestions, 1, add_to_list)
+        vim.ui.select(suggestions, { prompt = "WurdSpelSuggest for " .. diag.word }, function(selected)
+            if selected then
+                if selected == add_to_list then
+                    vim.cmd("spellgood " .. diag.word)
+                    M.spell_check_buffer()
+                else
+                    local bufn = vim.api.nvim_get_current_buf()
+                    vim.api.nvim_buf_set_text(bufn, diag.lnum, diag.col, diag.lnum,
+                        diag.end_col, { selected })
+                end
+            end
+        end)
+    else
+        vim.notify("No WurdSpel diagnostic under cursor.")
+    end
+end
+
+function M.toggle()
+    M.config.enabled = not M.config.enabled
+    if M.config.enabled then
+        M.spell_check_buffer()
+    else
+        vim.diagnostic.reset(M.ns_id)
+    end
+end
+
+function def_commands()
+    vim.api.nvim_create_user_command("WurdSpelBuf", M.spell_check_buffer, {})
+    vim.api.nvim_create_user_command("WurdSpelToggle", M.toggle, {})
+    vim.api.nvim_create_user_command("WurdSpelGood", M.spellgood, {})
+    vim.api.nvim_create_user_command("WurdSpelBad", M.spellbad, {})
+    vim.api.nvim_create_user_command("WurdSpelSuggest", M.spellsuggest, {})
+
+    if M.config.remap then
+        vim.keymap.set("n", "z=", M.spellsuggest)
+        vim.keymap.set("n", "zg", M.spellgood)
+        vim.keymap.set("n", "zw", M.spellbad)
+    end
+
+    if M.config.remap_special then
+        vim.keymap.set("n", "<leader>zz", M.spellsuggest)
+        vim.keymap.set("n", "<leader>zw", M.spellbad)
+        vim.keymap.set("n", "<leader>zg", M.spellgood)
+    end
+end
+
 function M.setup(user_config)
+    def_commands()
     local ns_id = vim.api.nvim_create_namespace("wurd_spel")
     M.config = vim.tbl_extend("force", M.config, user_config or {})
     M.config.ns_id = ns_id
@@ -99,14 +193,18 @@ function M.setup(user_config)
                     M.attached_bufs[bufn] = true
 
                     vim.api.nvim_buf_attach(0, false, {
-                        on_lines = function(_, buf, _, first, last, new_last, _)
-                            local lines = vim.api.nvim_buf_get_lines(buf, first, new_last, false)
-                            M.check_string_content(bufn, lines, first)
+                        on_lines = function(_, buf, _, first, _, new_last, _)
+                            if M.config.enabled then
+                                local lines = vim.api.nvim_buf_get_lines(buf, first, new_last, false)
+                                M.check_string_content(bufn, lines, first)
+                            end
                         end
                     })
                 end
 
-                M.spell_check_buffer()
+                if M.config.enabled then
+                    M.spell_check_buffer()
+                end
             end
         end
     })
